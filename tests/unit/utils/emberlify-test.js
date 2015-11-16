846228dc23d7ba21b5afdf1536ef1c6aa80809cc
Maciej Kwaśniak <kmaciek17@gmail.com>
2015-11-16 10:07:49 +0100

    Emberlify

---
+++ tests/unit/utils/emberlify-test.js
@@ -0,0 +1,9 @@
+import urlify from '../../../utils/emberlify';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | emberlify');
+
+test('it emberlify string', function(assert) {
+  assert.equal(emberlify('oko Jest S12uper'), 'oko-jest-s12uper');
+  assert.equal(emberlify('Tu nie Ma , przecinka'), 'tu-nie-ma-przecinka');
+});
--- tests/unit/utils/urlify-test.js
+++
@@ -1,9 +0,0 @@
-import urlify from '../../../utils/urlify';
-import { module, test } from 'qunit';
-
-module('Unit | Utility | urlify');
-
-test('it urlify string', function(assert) {
-  assert.equal(urlify('oko Jest S12uper'), 'oko-jest-s12uper');
-  assert.equal(urlify('Tu nie Ma , przecinka'), 'tu-nie-ma-przecinka');
-});
